66c7564919abbfe9260565d1637367c9ca0701ed
luolihao-aicode <luolihao-aicode@users.noreply.github.com>
2026-08-04 22:43:56 +0800

    release: prepare Windows 0.1.2

--- lib/core/update_checker.dart
+++ lib/core/update_checker.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
-const currentWindowsVersion = '0.1.1';
+const currentWindowsVersion = '0.1.2';
 const windowsReleaseApi =
     'https://api.github.com/repos/luolihao-ios/iPhoneVpnclient/releases?per_page=30';
 
